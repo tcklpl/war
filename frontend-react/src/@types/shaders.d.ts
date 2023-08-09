9ac1c135455e9fa4e0aa2d10f4590c25a6279d35
tcklpl <negroniluan@gmail.com>
2023-08-08 22:31:45 -0300

    removing unused GLSL ES shaders

--- frontend-react/src/@types/shaders.d.ts
+++ frontend-react/src/@types/shaders.d.ts
@@ -1,16 +1,7 @@
 /**
- * This file exists to make it possible to import .vs and .fs files as modules like:
- *  import vertexShader from "./pbr.vs"
- *  import fragmentShader from "./pbr.fs"
+ * This file exists to make it possible to import .wgsl files as modules like:
+ *  import shaderSource from "./pbr.wgsl"
  */
-declare module '*.vs' {
-    const content: string;
-    export default content;
-}
-declare module '*.fs' {
-    const content: string;
-    export default content;
-}
 declare module '*.wgsl' {
     const content: string;
     export default content;
