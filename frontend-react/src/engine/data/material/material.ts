72987232116cbff9e374050a3833364bc5c3cc8c
tcklpl <negroniluan@gmail.com>
2023-08-08 22:53:54 -0300

    material support and pbr material

--- frontend-react/src/engine/data/material/material.ts
+++ frontend-react/src/engine/data/material/material.ts
@@ -1,10 +1,26 @@
+import { PBRShader } from "../../../shaders/pbr/pbr_shader";
 
 export abstract class Material {
+
+    private _id: number;
     
     constructor(private _name: string) {
+        this._id = game.engine.managers.material.requestMaterialId();
+    }
+
+    get id() {
+        return this._id;
     }
 
     get name() {
         return this._name;
     }
+
+    abstract get bindBroup(): GPUBindGroup;
+
+    bind(passEncoder: GPURenderPassEncoder) {
+        passEncoder.setBindGroup(PBRShader.UNIFORM_BINDING_GROUPS.FRAGMENT_MATERIAL, this.bindBroup);
+    }
+
+    abstract free(): void;
 }
