38507c1e1a93d09b3c08b72ea04ac6ac3b34a2db
tcklpl <negroniluan@gmail.com>
2023-08-27 12:12:00 -0300

    pending second stage

---
+++ game/src/engine/render/vanilla/render_stages/rs1_lights.ts
@@ -0,0 +1,18 @@
+import { RenderResourcePool } from "../render_resource_pool";
+import { RenderStage } from "./render_stage";
+
+export class RenderStageLights implements RenderStage {
+
+    async initialize() {
+        // TODO
+    }
+
+    render(pool: RenderResourcePool) {
+        // TODO
+    }
+
+    free() {
+        // TODO
+    }
+
+}
