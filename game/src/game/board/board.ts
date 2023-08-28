315af0a964250e85d13c618b81e38cff500b7487
tcklpl <negroniluan@gmail.com>
2023-08-28 12:37:29 -0300

    proper board scene

--- game/src/game/board/board.ts
+++ game/src/game/board/board.ts
@@ -2,24 +2,37 @@ import { Scene } from "../../engine/data/scene/scene";
 import { BoardCamera } from "./board_camera";
 import { BoardCountriesIndex } from "./board_countries_index";
 import { BoardLights } from "./board_lights";
+import { BoardSkybox } from "./board_skybox";
 
 export class GameBoard extends Scene {
 
     private _mainCamera: BoardCamera;
     private _countries: BoardCountriesIndex;
     private _boardLights: BoardLights;
+    private _skybox: BoardSkybox;
 
     constructor() {
         const mainCamera = new BoardCamera()
         const countries = new BoardCountriesIndex();
         const lights = new BoardLights();
+        const skybox = new BoardSkybox();
         
-        super('Game board scene', [...countries.allCountries], [ mainCamera ], [ lights.sun ]);
+        super('Game board scene', {
+            entities: [ ...countries.allCountries ],
+            cameras: [ mainCamera ],
+            lights: [ lights.sun ],
+            skyboxes: [ skybox ]
+        });
         this._mainCamera = mainCamera;
         this._countries = countries;
         this._boardLights = lights;
+        this._skybox = skybox;
         this.activeCamera = this._mainCamera;
-        
+        this.activeSkybox = this._skybox;
+    }
+
+    async initialize() {
+        await this._skybox.initialize();
     }
 
 }
