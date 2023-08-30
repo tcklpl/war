5a4b85bd083a22f1d12dec2cc9d7eb35074f1ce3
tcklpl <negroniluan@gmail.com>
2023-08-30 15:17:58 -0300

    brdf integral (lut) shader

---
+++ game/src/shaders/brdf_convolution_integral/brdf_convolution_integral_shader.ts
@@ -0,0 +1,14 @@
+import { Shader } from "../shader";
+import shaderSource from "./brdf_convolution_integral.wgsl";
+
+export class BRDFConvolutionIntegralShader extends Shader {
+
+    static UNIFORM_BINDING_GROUPS = {
+    }
+
+    constructor(name: string, cb: () => void) {
+        super(name);
+        this.compileShader(shaderSource).then(() => cb());
+    }
+
+}
