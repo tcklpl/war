8e0af348b8dc83e16849dae6ff752d34872656b9
tcklpl <negroniluan@gmail.com>
2023-08-31 14:14:04 -0300

    changed prefiltered skybox resolution

--- game/src/game/board/board_skybox.ts
+++ game/src/game/board/board_skybox.ts
@@ -3,13 +3,13 @@ import { TextureUtils } from "../../utils/texture_utils";
 
 export class BoardSkybox extends Skybox {
 
-    private _skyImage = game.engine.managers.asset.getHDRAsset('kloofendal_43d_clear');
+    private _skyImage = game.engine.managers.asset.getHDRAsset('thatch_chapel');
 
     async initialize() {
         const bitmap = await this._skyImage.toBitmap();
         const equirecTex = await TextureUtils.createRGBA16fFromHDRBitmap(bitmap, this._skyImage.width, this._skyImage.height);
         this.skybox = await game.engine.utilRenderers.equirecToCubemap.renderEquirectangularMapToCubemap(equirecTex, 1024);
-        this.convolutedSkybox = await game.engine.utilRenderers.cubemapConvolution.convoluteCubemap(this.skybox, 128);
+        this.convolutedSkybox = await game.engine.utilRenderers.cubemapConvolution.convoluteCubemap(this.skybox, 256);
         this.prefilteredSkybox = await game.engine.utilRenderers.cubemapPrefilter.prefilterCubemap(this.skybox);
         equirecTex.destroy();
     }
