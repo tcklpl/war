44ff2656eafe0a657d4a953a240b6ee26057f9d7
tcklpl <negroniluan@gmail.com>
2023-09-11 15:55:39 -0300

    id pool now registers identifiable entities

--- game/src/engine/data/traits/identifiable.ts
+++ game/src/engine/data/traits/identifiable.ts
@@ -1,28 +1,28 @@
 import { Constructor } from "typeUtils";
-import { Vec4 } from "../vec/vec4";
 
-interface IIdentifiable {
+export interface IIdentifiable {
     get id(): number;
+    get idUint32(): Uint32Array;
 }
 
 export function identifiable<T extends Constructor>(base: T): Constructor<IIdentifiable> & T {
     return class extends base {
 
         private _id: number;
-        private _idVec4: Vec4;
+        private _idUint32: Uint32Array;
 
         constructor(...args: any[]) {
             super(...args);
-            this._id = game.engine.idPool.requestID();
-            this._idVec4 = Vec4.fromId(this._id);
+            this._id = game.engine.idPool.requestID(this);
+            this._idUint32 = new Uint32Array([this._id]);
         }
 
         get id() {
             return this._id;
         }
 
-        get idVec4() {
-            return this._idVec4;
+        get idUint32() {
+            return this._idUint32;
         }
     }
 }
--- game/src/engine/identifier_pool.ts
+++ game/src/engine/identifier_pool.ts
@@ -1,10 +1,18 @@
+import { IIdentifiable } from "./data/traits/identifiable";
 
 export class IdentifierPool {
 
     private _currentID: number = 0;
+    private _indentifiables: Map<number, IIdentifiable> = new Map();
 
-    requestID() {
+    requestID(i: IIdentifiable) {
+        const id = this._currentID++;
+        this._indentifiables.set(id, i);
         return this._currentID++;
+    }
+
+    get(id: number) {
+        return this._indentifiables.get(id);
     }
 
 }
