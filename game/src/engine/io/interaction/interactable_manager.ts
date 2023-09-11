d7eb795aa296e30e5b3f3306d09377e4bb097b03
tcklpl <negroniluan@gmail.com>
2023-09-11 15:57:45 -0300

    interaction manager

---
+++ game/src/engine/io/interaction/interactable_manager.ts
@@ -0,0 +1,19 @@
+import { IIdentifiable } from "../../data/traits/identifiable";
+import { IInteractable } from "../../data/traits/interactable";
+
+export class InteractableManager {
+
+    private _interactables: Map<number, IInteractable> = new Map();
+
+    registerInteractable(i: IInteractable & IIdentifiable) {
+        this._interactables.set(i.id, i);
+    }
+
+    unregisterInteractable(i: IInteractable & IIdentifiable) {
+        return this._interactables.delete(i.id);
+    }
+
+    get(id: number) {
+        return this._interactables.get(id);
+    }
+}
---
+++ game/src/engine/io/interaction/mouse_interaction_manager.ts
@@ -0,0 +1,36 @@
+import { Mouse } from "../mouse";
+
+export class MouseInteractionManager {
+
+    constructor(private _mouse: Mouse) {
+        _mouse.registerListener(this);
+    }
+
+    private _currentHoverID = 0;
+
+    notifyFramePickingID(id: number) {
+        if (id === this._currentHoverID) return;
+
+        const old = this.currentHovered;
+        if (old?.onMouseLeave) old.onMouseLeave();
+
+        this._currentHoverID = id;
+
+        const current = this.currentHovered;
+        if (current?.onMouseHover) current.onMouseHover();
+    }
+
+    onMouseLeftClick() {
+        const current = this.currentHovered;
+        if (current?.onMouseLeftClick) current.onMouseLeftClick();
+    }
+
+    onMouseRightClick() {
+        const current = this.currentHovered;
+        if (current?.onMouseRightClick) current.onMouseRightClick();
+    }
+
+    get currentHovered() {
+        return game.engine.managers.io.interactionManager.get(this._currentHoverID);
+    }
+}
