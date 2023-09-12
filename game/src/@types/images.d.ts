68182e0f9995c7f7e087cfb4791cd0fd7a2a3287
tcklpl <negroniluan@gmail.com>
2023-09-12 12:01:36 -0300

    image module declaration

---
+++ game/src/@types/images.d.ts
@@ -0,0 +1,8 @@
+/**
+ * This file exists to make it possible to import image files inside react components.
+ */
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.avif';
