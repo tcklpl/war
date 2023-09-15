c926bd019c1d9921e361c21f180aecaf58cedad4
tcklpl <negroniluan@gmail.com>
2023-09-15 13:43:45 -0300

    correctly rendering velocity texture

--- game/src/engine/render/vanilla/render_stages/rs_depth_map.ts
+++ game/src/engine/render/vanilla/render_stages/rs_depth_map.ts
@@ -1,12 +1,11 @@
-import { DepthShader } from "../../../../shaders/depth/depth_shader";
-import { Shader } from "../../../../shaders/shader";
+import { DepthAndVelocityShader } from "../../../../shaders/depth/depth_and_velocity_shader";
 import { RenderInitializationResources } from "../render_initialization_resources";
 import { RenderResourcePool } from "../render_resource_pool";
 import { RenderStage } from "./render_stage";
 
 export class RenderStageDepthMap implements RenderStage {
     
-    private _depthShader!: Shader;
+    private _depthShader!: DepthAndVelocityShader;
     private _depthPipelineCW!: GPURenderPipeline;
     private _depthPipelineCCW!: GPURenderPipeline;
     private _renderPassDescriptor!: GPURenderPassDescriptor;
@@ -35,7 +34,7 @@ export class RenderStageDepthMap implements RenderStage {
     async initialize(resources: RenderInitializationResources) {
 
         await new Promise<void>(r => {
-            this._depthShader = new DepthShader('rs depth shader', () => r());
+            this._depthShader = new DepthAndVelocityShader('rs depth and velocity shader', () => r());
         });
 
         this._depthPipelineCW = await this.createDepthPipeline('cw');
@@ -65,7 +64,9 @@ export class RenderStageDepthMap implements RenderStage {
             fragment: {
                 module: this._depthShader.module,
                 entryPoint: 'fragment',
-                targets: []
+                targets: [
+                    { format: 'rg16float' as GPUTextureFormat }
+                ]
             },
             primitive: {
                 topology: 'triangle-list',
@@ -82,7 +83,14 @@ export class RenderStageDepthMap implements RenderStage {
 
     private createRenderPassDescriptor() {
         return {
-            colorAttachments: [] as GPURenderPassColorAttachment[],
+            colorAttachments: [
+                // Velocity
+                {
+                    // view will be assigned later
+                    loadOp: 'clear',
+                    storeOp: 'store',
+                }
+            ] as GPURenderPassColorAttachment[],
             depthStencilAttachment: {
                 // view will be assigned later
                 depthClearValue: 1,
@@ -96,11 +104,15 @@ export class RenderStageDepthMap implements RenderStage {
         const pipeline = windingOrder === 'ccw' ? this._depthPipelineCCW : this._depthPipelineCW;
         return device.createBindGroup({
             label: 'PBR ViewProj',
-            layout: pipeline.getBindGroupLayout(DepthShader.UNIFORM_BINDING_GROUPS.VERTEX_VIEWPROJ),
+            layout: pipeline.getBindGroupLayout(DepthAndVelocityShader.BINDING_GROUPS.VIEWPROJ),
             entries: [
                 { binding: 0, resource: { buffer: buffer }}
             ]
         });
+    }
+
+    private setVelocityTexture(velocityTex: GPUTextureView) {
+        (this._renderPassDescriptor.colorAttachments as GPURenderPassColorAttachment[])[0].view = velocityTex;
     }
 
     private setDepthTexture(depthTex: GPUTextureView) {
@@ -111,17 +123,18 @@ export class RenderStageDepthMap implements RenderStage {
         
         pool.commandEncoder.pushDebugGroup('Depth Map Renderer');
         this.setDepthTexture(pool.depthTextureView);
+        this.setVelocityTexture(pool.velocityTextureView);
         const rpe = pool.commandEncoder.beginRenderPass(this._renderPassDescriptor);
 
         if (pool.scene.entitiesPerWindingOrder.ccw.length > 0) {
             rpe.setPipeline(this._depthPipelineCCW);
-            rpe.setBindGroup(DepthShader.UNIFORM_BINDING_GROUPS.VERTEX_VIEWPROJ, this._viewProjBindGroupCCW);
+            rpe.setBindGroup(DepthAndVelocityShader.BINDING_GROUPS.VIEWPROJ, this._viewProjBindGroupCCW);
             pool.scene.entitiesPerWindingOrder.ccw.forEach(e => e.draw(rpe, this._depthPipelineCCW, this._meshDrawOptions));
         }
 
         if (pool.scene.entitiesPerWindingOrder.cw.length > 0) {
             rpe.setPipeline(this._depthPipelineCW);
-            rpe.setBindGroup(DepthShader.UNIFORM_BINDING_GROUPS.VERTEX_VIEWPROJ, this._viewProjBindGroupCW);
+            rpe.setBindGroup(DepthAndVelocityShader.BINDING_GROUPS.VIEWPROJ, this._viewProjBindGroupCW);
             pool.scene.entitiesPerWindingOrder.cw.forEach(e => e.draw(rpe, this._depthPipelineCW, this._meshDrawOptions));
         }
 
--- game/src/shaders/depth/depth_and_velocity.wgsl
+++ game/src/shaders/depth/depth_and_velocity.wgsl
@@ -16,7 +16,7 @@ struct VSCommonUniforms {
     projection: mat4x4f,
     previous_projection: mat4x4f,
     camera_position: vec3f,
-    jitter: vec2f
+    jitter: vec2f,
 };
 @group(0) @binding(0) var<uniform> vsCommonUniforms: VSCommonUniforms;
 
@@ -46,23 +46,36 @@ fn vertex(@location(0) position: vec3f) -> VSOutput {
     var viewPos  = vsCommonUniforms.camera * worldPos;
     var pos = vsCommonUniforms.projection * viewPos;
 
-    pos += vec4f(vsCommonUniforms.jitter, 0.0, 0.0) * pos.w;
+    var pos_jittered = pos + vec4f(vsCommonUniforms.jitter, 0.0, 0.0) * pos.w;
 
     var prevWorldPos = vsUniqueUniforms.previous_model * vec4f(position, 1.0);
     var prevViewPos  = vsCommonUniforms.previous_camera * prevWorldPos;
     var prevPos = vsCommonUniforms.previous_projection * prevViewPos;
 
     var output: VSOutput;
-    output.position = pos;
+    output.position = pos_jittered;
     output.prev_position = prevPos;
     output.cur_position = pos;
 
     return output;
 }
 
+fn calculateVelocity(newPos: vec4f, oldPos: vec4f) -> vec2f {
+    var oldP = oldPos;
+    oldP /= oldP.w;
+    oldP = (oldP + 1.0) / 2.0;
+    oldP.y = 1.0 - oldP.y;
+
+    var newP = newPos;
+    newP /= newP.w;
+    newP = (newP + 1.0) / 2.0;
+    newP.y = 1.0 - newP.y;
+
+    return (newP - oldP).xy;
+}
+
 @fragment
 fn fragment(v: VSOutput) -> @location(0) vec2f {
-    var a = (v.cur_position.xy / v.cur_position.w) * 0.5 + 0.5;
-    var b = (v.prev_position.xy / v.prev_position.w) * 0.5 + 0.5;
-    return (a - b);
+    var velocity = calculateVelocity(v.cur_position, v.prev_position);
+    return velocity;
 }
