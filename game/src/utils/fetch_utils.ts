5955a0c20454fed121d4a774560d531af3bc62a8
tcklpl <negroniluan@gmail.com>
2023-09-16 11:14:56 -0300

    made the gltf buffer into an ArrayBuffer

--- game/src/utils/fetch_utils.ts
+++ game/src/utils/fetch_utils.ts
@@ -4,7 +4,7 @@ export class FetchUtils {
     static async fetchByteBuffer(uri: string) {
         const response = await fetch(uri);
         const data = await response.arrayBuffer();
-        return new Uint8Array(data);
+        return data;
     }
 
 }
