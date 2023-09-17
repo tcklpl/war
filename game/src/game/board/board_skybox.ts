fb11169a33c04ff432074b8da09a33387eb48b82
tcklpl <negroniluan@gmail.com>
2023-09-17 10:51:40 -0300

    updated board skybox to use the hdr asset

--- game/src/game/board/board_skybox.ts
+++ game/src/game/board/board_skybox.ts
@@ -3,12 +3,11 @@ import { TextureUtils } from "../../utils/texture_utils";
 
 export class BoardSkybox extends Skybox {
 
-    private _skyImage = game.engine.managers.asset.getHDRAsset('thatch_chapel');
+    private _skyImage = game.engine.managers.asset.getHDRAsset('thatch_chapel_4k');
 
     async initialize() {
-        const bitmap = await this._skyImage.toBitmap();
-        const equirecTex = await TextureUtils.createRGBA16fFromHDRBitmap(bitmap, bitmap.width, bitmap.height);
-        this.skybox = await game.engine.utilRenderers.equirecToCubemap.renderEquirectangularMapToCubemap(equirecTex, 1024);
+        const equirecTex = await TextureUtils.createRGBA32fFromRGBEData(this._skyImage.data);
+        this.skybox = await game.engine.utilRenderers.equirecToCubemap.renderEquirectangularMapToCubemap(equirecTex, 'rgba32float', 1024);
         this.convolutedSkybox = await game.engine.utilRenderers.cubemapConvolution.convoluteCubemap(this.skybox, 256);
         this.prefilteredSkybox = await game.engine.utilRenderers.cubemapPrefilter.prefilterCubemap(this.skybox);
         equirecTex.destroy();
