11852a41a88736a4724e9106f9a15f2088686306
tcklpl <negroniluan@gmail.com>
2023-09-20 12:25:20 -0300

    db v1: asset caching

---
+++ game/src/engine/idb/versions/v1_asset_cache.ts
@@ -0,0 +1,11 @@
+import { IDBDBVersion } from "./idb_db_version";
+
+export class IDBv1AssetCache implements IDBDBVersion {
+
+    version = 1;
+
+    migrate(db: IDBDatabase): void {
+        db.createObjectStore('asset-cache', {keyPath: 'name'});
+    }
+    
+}
