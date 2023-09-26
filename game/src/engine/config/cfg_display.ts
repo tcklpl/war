6e0b03ea512631a23c637d96ddbfff73bad94707
tcklpl <negroniluan@gmail.com>
2023-09-25 22:48:25 -0300

    config setting for future localization support

--- game/src/engine/config/cfg_display.ts
+++ game/src/engine/config/cfg_display.ts
@@ -3,5 +3,6 @@ import { ConfigPage } from "./cfg_page";
 export class ConfigDisplay implements ConfigPage {
     page = 'display';
     theme: string = 'dark';
+    language: string = 'en-US';
     showPerformance: boolean = false;
 }
