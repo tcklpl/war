70d2b950de527b2f3d153f47a551c5273b6e26f5
tcklpl <negroniluan@gmail.com>
2023-10-04 17:04:06 -0300

    shadow map engine config

--- game/src/engine/config/cfg_graphics.ts
+++ game/src/engine/config/cfg_graphics.ts
@@ -2,6 +2,19 @@ import { ConfigPage } from "./cfg_page";
 
 export class ConfigGraphics implements ConfigPage {
     page = 'graphics';
+
+    // rendering
+    /**
+     * Shadow map quality, possible values:
+     * 0    Off         --
+     * 1    Very Low    512
+     * 2    Low         1024
+     * 3    Medium      2048
+     * 4    High        4096
+     */
+    shadowMapQuality: number = 3;
+
+    // post processing
     useSSAO: boolean = true;
     useBloom: boolean = true;
     useTAA: boolean = true;
