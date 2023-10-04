a2efe93667540ff7a7a9b242f1b1f042b1504408
tcklpl <negroniluan@gmail.com>
2023-10-04 17:02:57 -0300

    matrix util to get frustum coordinates

---
+++ game/src/utils/matrix_utils.ts
@@ -0,0 +1,30 @@
+import { Mat4 } from "../engine/data/mat/mat4";
+import { Vec4 } from "../engine/data/vec/vec4";
+
+export class MatrixUtils {
+
+    static getFrustumCornersWorldSpace(projection: Mat4, view: Mat4) {
+
+        const inv = projection.duplicate().multiplyByMat4(view).inverse();
+        const corners: Vec4[] = [];
+
+        for (let x = 0; x < 2; x++) {
+            for (let y = 0; y < 2; y++) {
+                for (let z = 0; z < 2; z++) {
+                    const point = new Vec4(
+                        2 * x - 1,
+                        2 * y - 1,
+                        2 * z - 1,
+                        1
+                    );
+                    const transformed = inv.multiplyByVec4(point).divideByFactor(point.w);
+                    corners.push(transformed);
+                }
+            }
+        }
+
+        return corners;
+
+    }
+
+}
