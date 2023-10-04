1bea3bb6834c1b9595417f117e1920eaf55596dc
tcklpl <negroniluan@gmail.com>
2023-10-04 16:59:49 -0300

    vec4 utils: min, max and centroid

--- game/src/engine/data/vec/vec4.ts
+++ game/src/engine/data/vec/vec4.ts
@@ -1,4 +1,5 @@
 import { BadVectorLengthError } from "../../../errors/engine/data/bad_vector_length";
+import { Vec3 } from "./vec3";
 import { Vector } from "./vector";
 
 export class Vec4 extends Vector {
@@ -43,6 +44,10 @@ export class Vec4 extends Vector {
         this._values[3] = val;
     }
 
+    get xyz() {
+        return new Vec3(this.x, this.y, this.z);
+    }
+
     // -----------------[ STATIC UTILS ]-----------------
 
     static fromId(id: number): Vec4 {
@@ -61,5 +66,38 @@ export class Vec4 extends Vector {
     static fromArray(a: number[]) {
         if (a.length !== 4) throw new BadVectorLengthError(`Trying to create vec4 with an array of ${a.length} elements`);
         return new Vec4(a[0], a[1], a[2], a[3]);
+    }
+
+    static min(v: Vec4[]) {
+        return v.reduce((prev, cur) => {
+            prev.x = Math.min(cur.x);
+            prev.y = Math.min(cur.y);
+            prev.z = Math.min(cur.z);
+            prev.w = Math.min(cur.w);
+            return prev;
+        }, Vec4.fromValue(0));
+    }
+
+    static max(v: Vec4[]) {
+        return v.reduce((prev, cur) => {
+            prev.x = Math.max(cur.x);
+            prev.y = Math.max(cur.y);
+            prev.z = Math.max(cur.z);
+            prev.w = Math.max(cur.w);
+            return prev;
+        }, Vec4.fromValue(0));
+    }
+
+    static add(a: Vec4, b: Vec4) {
+        return new Vec4(
+            a.x + b.x,
+            a.y + b.y,
+            a.z + b.z,
+            a.w + b.w
+        );
+    }
+
+    static centroid(v: Vec4[]) {
+        return v.reduce((accumulated, current) => Vec4.add(accumulated, current), Vec4.fromValue(0)).divideByFactor(v.length);
     }
 }
