f9ca7289fd067f77c827789b8d88739352735efc
tcklpl <negroniluan@gmail.com>
2023-10-04 17:03:26 -0300

    generalized lights to support shadow maps

--- game/src/engine/data/lights/light.ts
+++ game/src/engine/data/lights/light.ts
@@ -1,36 +1,50 @@
+import { MappedAtlasRegion } from "../atlas/mapped_atlas_region";
+import { MappedRegionSize } from "../atlas/mapped_region_size";
+import { Mat4 } from "../mat/mat4";
 import { Vec3 } from "../vec/vec3";
+
+export interface LightProperties {
+    name: string;
+    position: Vec3;
+    color: Vec3;
+    intensity: number;
+    range: number;
+
+    /**
+     * If the light can cast shadows.
+     */
+    castsShadows: boolean;
+    /**
+     * Required shadow map resolution.
+     */
+    shadowMapSize: MappedRegionSize;
+    /**
+     * If the light source can accept having it's shadow map resolution smaller
+     * than the specified at "shadowMapSize".
+     */
+    shadowMapCanShrink: boolean;
+}
 
 export abstract class Light {
 
-    constructor(
-        private _name: string, 
-        private _position: Vec3,
-        private _color: Vec3, 
-        private _intensity: number, 
-        private _range: number, 
-        private _enabled: boolean = true
-        ) {
+    private _properties: LightProperties;
+    /**
+     * Shadow map atlas region if shadow mapping is enabled.
+     * 
+     * If shadow mapping is enabled and this value is undefined (as it will be on the first frame),
+     * the "light" render stage will try to allocate a spot on the shadow map atlas and overwrite this
+     * attribute.
+     */
+    shadowAtlasMappedRegion?: MappedAtlasRegion;
+    shadowMappingViewProj?: Mat4;
 
+    constructor(properties: LightProperties, private _enabled = true) {
+        this._properties = properties;
+        game.engine.managers.light.register(this);
     }
 
-    get name() {
-        return this._name;
-    }
-
-    get position() {
-        return this._position;
-    }
-
-    get color() {
-        return this._color;
-    }
-
-    get intensity() {
-        return this._intensity;
-    }
-
-    get range() {
-        return this._range;
+    get properties() {
+        return this._properties;
     }
 
     get enabled() {
@@ -38,7 +52,7 @@ export abstract class Light {
     }
 
     set enabled(e: boolean) {
-        this._enabled = this.enabled;
+        this._enabled = e;
     }
 
     abstract writeToBuffer(buf: GPUBuffer, index: number, generalBufferOffset: number): void;    
