9a915fe33e33d8ac2fc89bbf5c813628c4e3c08a
tcklpl <negroniluan@gmail.com>
2023-10-10 18:31:29 -0300

    commented and simplified skybox class

--- game/src/engine/data/skybox/skybox.ts
+++ game/src/engine/data/skybox/skybox.ts
@@ -14,10 +14,7 @@ export abstract class Skybox {
         addressModeV: 'clamp-to-edge',
         addressModeW: 'clamp-to-edge'
     });
-    private _skybox = new Texture();
     private _prefilteredSkybox = new Texture();
-    private _prefilteredSkyboxMip0Resolution = 1024;
-
     private _pipelineBindGroups = new Map<GPURenderPipeline, {skybox: GPUBindGroup}>();
 
     abstract initialize(): Promise<void>;
@@ -31,7 +28,7 @@ export abstract class Skybox {
             layout: pipeline.getBindGroupLayout(SkyboxShader.BINDING_GROUPS.TEXTURE),
             entries: [
                 { binding: 0, resource: this._sampler },
-                { binding: 1, resource: this._skybox.texture.createView({ dimension: 'cube' }) }
+                { binding: 1, resource: this._prefilteredSkybox.texture.createView({ dimension: 'cube' }) }
             ]
         });
 
@@ -43,40 +40,45 @@ export abstract class Skybox {
         return res;
     }
 
+    /**
+     * Constructs a skybox texture and pre-filters the produced cubemap from an equirectangular HDR asset.
+     * 
+     * This function essentially creates the skybox.
+     * 
+     * @param hdrAsset HDR Asset from the Asset Manager.
+     * @param resolution Desired cubemap resolution.
+     */
     async setSkyboxFromHDRAsset(hdrAsset: HDRAsset, resolution = 1024) {
         const equirecTex = new Texture(await TextureUtils.createRGBA32fFromRGBEData(hdrAsset.data));
-        await this.setSkyboxFromEquirectangularProjection(equirecTex, 'rgba32float', resolution);
+        await this.setSkyboxFromEquirectangularProjection(equirecTex, resolution);
         equirecTex.free();
     }
 
-    async setSkyboxFromEquirectangularProjection(equirecTex: Texture, texType: 'rgba16float' | 'rgba32float', resolution = 1024) {
+    /**
+     * Generates a `rgba16float` cubemap (sized [resolution, resolution, 6]) from an equirectangular projection texture.
+     * @param equirecTex Equirectangular projection texture.
+     * @param resolution Final skybox resolution.
+     */
+    async setSkyboxFromEquirectangularProjection(equirecTex: Texture, resolution = 1024) {
         const cubemapTex = new Texture(
             await game.engine.utilRenderers.equirecToCubemap.renderEquirectangularMapToCubemap(equirecTex.texture, {
-                texType: texType,
                 cubemapResolution: resolution,
                 mipCount: 10
             })
         );
         await cubemapTex.generateBitmaps();
-        await this.setSkyboxCubemap(cubemapTex);
+        await this.prefilterSkybox(cubemapTex);
+        // the cubemap can be discarded as we can use the first mip of the pre-filtered one as the skybox
+        cubemapTex.free();
     }
 
-    async setSkyboxCubemap(skyboxCubemap: Texture) {
-        this.skybox = skyboxCubemap;
-        await this.prefilterSkybox();
-    }
-
-    async prefilterSkybox() {
-        this._prefilteredSkybox.texture = await game.engine.utilRenderers.cubemapPrefilter.prefilterCubemap(this._skybox.texture, this._prefilteredSkyboxMip0Resolution);
-    }
-
-    private set skybox(skybox: Texture) {
-        this._skybox.free();
-        this._skybox = skybox;
-    }
-
-    get skybox() {
-        return this._skybox;
+    /**
+     * Pre-filters the skybox cubemap using the util renderer.
+     * @param skybox Cubemap skybox texture with at least 10 mips.
+     */
+    async prefilterSkybox(skybox: Texture) {
+        // the cubemap resolution is going to be the width as we want the first layer to be a copy of the skybox.
+        this._prefilteredSkybox.texture = await game.engine.utilRenderers.cubemapPrefilter.prefilterCubemap(skybox.texture, skybox.texture.width);
     }
 
     get prefilteredSkybox() {
@@ -92,7 +94,6 @@ export abstract class Skybox {
     }
 
     free() {
-        this._skybox.free();
         this._prefilteredSkybox.free();
     }
     
