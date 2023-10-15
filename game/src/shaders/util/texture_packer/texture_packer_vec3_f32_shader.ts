5696952ab87c7a07f56bb4c1d6656ff38e455641
tcklpl <negroniluan@gmail.com>
2023-10-15 10:48:01 -0300

    vec3 + f32 texture packer

---
+++ game/src/shaders/util/texture_packer/texture_packer_vec3_f32_shader.ts
@@ -0,0 +1,15 @@
+import { Shader } from "../../shader";
+import shaderSource from "./texture_packer_vec3_f32.wgsl";
+
+export class TexturePackerVec3f32Shader extends Shader {
+
+    static BINDING_GROUPS = {
+        TEXTURE: 0
+    }
+
+    constructor(name: string, cb: () => void) {
+        super(name);
+        this.compileShader(shaderSource).then(() => cb());
+    }
+
+}
