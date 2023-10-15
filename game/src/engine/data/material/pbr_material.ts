04a1c157ff25a361458729d11d4fd1aa5b3a576a
tcklpl <negroniluan@gmail.com>
2023-10-15 10:49:28 -0300

    proper interface for PBR materials

--- game/src/engine/data/material/pbr_material.ts
+++ game/src/engine/data/material/pbr_material.ts
@@ -1,5 +1,6 @@
 import { PrincipledBSDFShader } from "../../../shaders/geometry/principled_bsdf/principled_bsdf_shader";
 import { TextureUtils } from "../../../utils/texture_utils";
+import { Texture } from "../texture/texture";
 import { Vec4 } from "../vec/vec4";
 import { Material } from "./material";
 
@@ -12,11 +13,28 @@ interface PBRMaterialProps {
 
 export class PBRMaterial extends Material {
 
-    private _albedo!: GPUTexture;
-    private _normal!: GPUTexture;
-    private _metallic!: GPUTexture;
-    private _roughness!: GPUTexture;
-    private _ao!: GPUTexture;
+    /*
+        PBR Material Information:
+
+        Texture Name    Format          Separation          Data Explanation
+        -------------------------------------------------------------------------------
+        Albedo          rgba16float     [r, g, b, a]¹       1: Material HDR color.
+
+        Normal          rgba8unorm      [r, g, b]¹ [a]²     1: Normal vector;
+                                                            2: AO.
+
+        Props 1         rgba8unorm      [r]¹ [g]² [b]³ [a]⁴ 1: Metallic;
+                                                            2: Roughness;
+                                                            3: Transmission;
+                                                            4: Transmission Roughness;
+
+        Props 2         r16float        [r]¹                1: IOR
+
+    */
+    private _albedo = new Texture();
+    private _normal_ao = new Texture();
+    private _props1 = new Texture();
+    private _props2 = new Texture();
 
     private _sampler!: GPUSampler;
 
@@ -27,21 +45,26 @@ export class PBRMaterial extends Material {
         this.constructTextures();
     }
 
-    private constructTextures() {
-        if (this._albedo) this._albedo.destroy();
-        this._albedo = TextureUtils.createTextureFromNormalizedVec4(this._props.baseColor);
+    private async constructTextures() {
+        this.free();
 
-        if (this._normal) this._normal.destroy();
-        this._normal = TextureUtils.createTextureFromNormalizedVec4(new Vec4(0.5, 0.5, 1.0, 1.0));
+        // TODO: Support texture maps
+        this._albedo.texture = TextureUtils.createTextureFromNormalizedVec4(this._props.baseColor);
 
-        if (this._metallic) this._metallic.destroy();
-        this._metallic = TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(this._props.metallic));
+        // TODO: Support normal and AO maps
+        const normal = new Texture(TextureUtils.createTextureFromNormalizedVec4(new Vec4(0.5, 0.5, 1.0, 1.0)));
+        const ao = new Texture(TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(1)));
+        this._normal_ao = await game.engine.utilRenderers.packing.pack_1rgb1a_rgba8unorm(normal, ao);
+        [normal, ao].forEach(t => t.free());
 
-        if (this._roughness) this._roughness.destroy();
-        this._roughness = TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(this._props.roughness));
-
-        if (this._ao) this._ao.destroy();
-        this._ao = TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(1));
+        const metallic = new Texture(TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(this._props.metallic)));
+        const roughness = new Texture(TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(this._props.roughness)));
+        const transmission = new Texture(TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(0)));
+        const transmissionRoughness = new Texture(TextureUtils.createTextureFromNormalizedVec4(Vec4.fromValue(0)));
+        this._props1 = await game.engine.utilRenderers.packing.pack_4r_rgba8unorm(metallic, roughness, transmission, transmissionRoughness);
+        [metallic, roughness, transmission, transmissionRoughness].forEach(t => t.free());
+        
+        this._props2 = new Texture(TextureUtils.create1pxR16Texture(this._props.ior));
 
         this._sampler = device.createSampler({
             label: `PBR Material '${this.name} sampler'`
@@ -58,11 +81,10 @@ export class PBRMaterial extends Material {
             layout: pipeline.getBindGroupLayout(PrincipledBSDFShader.BINDING_GROUPS.MATERIAL),
             entries: [
                 { binding: 0, resource: this._sampler },
-                { binding: 1, resource: this._albedo.createView() },
-                { binding: 2, resource: this._normal.createView() },
-                { binding: 3, resource: this._metallic.createView() },
-                { binding: 4, resource: this._roughness.createView() },
-                { binding: 5, resource: this._ao.createView() },
+                { binding: 1, resource: this._albedo.view },
+                { binding: 2, resource: this._normal_ao.view },
+                { binding: 3, resource: this._props1.view },
+                { binding: 4, resource: this._props2.view }
             ]
         });
         this._pipelineBindGroups.set(pipeline, newBindGroup);
@@ -70,11 +92,10 @@ export class PBRMaterial extends Material {
     }
 
     free() {
-        this._albedo?.destroy();
-        this._normal?.destroy();
-        this._metallic?.destroy();
-        this._roughness?.destroy();
-        this._ao?.destroy();
+        this._albedo.free();
+        this._normal_ao.free();
+        this._props1.free();
+        this._props2.free();
     }
 
 }
