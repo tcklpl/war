63a852fc3d53d9aee31d23d168b530c891d4f12f
tcklpl <negroniluan@gmail.com>
2023-10-20 18:29:11 -0300

    protocol interfaces

---
+++ protocol/index.ts
@@ -0,0 +1,3 @@
+export * from './src/rest/sv_server_info';
+export * from './src/rest/sv_login_response_ok';
+export * from './src/rest/cl_login_request';
---
+++ protocol/src/rest/sv_server_info.ts
@@ -0,0 +1,5 @@
+
+export interface sv_ServerInfo {
+    name: string;
+    hasPassword: boolean;
+}
