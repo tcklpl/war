93f5384186c0b884153552b004b91ed9e350b45a
tcklpl <negroniluan@gmail.com>
2024-01-04 18:18:09 -0300

    added cors to the server

--- server/src/express/express_server.ts
+++ server/src/express/express_server.ts
@@ -1,5 +1,6 @@
 import { ConfigManager } from "../config/config_manager";
 import express from 'express';
+import cors from 'cors';
 import { CfgServer } from "../config/default/cfg_server";
 import { ExpressRoutes } from "./routes/routes";
 import { CryptManager } from "../crypt/crypt_manager";
@@ -16,6 +17,7 @@ export class ExpressServer {
         const serverConfig = this._configManager.getConfig(CfgServer);
         
         this._app.use(express.json());
+        this._app.use(cors());
         this._routes.initialize();
         this._app.use(this._routes.routes.map(r => r.router));
 
