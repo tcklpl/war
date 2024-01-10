6ac58f1ffc8a808c7eeca59573e4e0698e2279fe
tcklpl <negroniluan@gmail.com>
2024-01-10 10:41:21 -0300

    game class server list

--- game/src/game/war_game.ts
+++ game/src/game/war_game.ts
@@ -1,13 +1,18 @@
 import { Engine } from "../engine/engine";
 import { GameBoard } from "./board/board";
 import { GameLoader } from "./loader/game_loader";
+import { ServerList } from "./server/server_list";
 
 export class WarGame {
 
     private _loader = new GameLoader();
     private _engine = new Engine();
+    
+    private _serverList = new ServerList();
 
     private _gameBoard!: GameBoard;
+    private _toRunWhenReady: (() => void)[] = [];
+    private _ready = false;
 
     static initialize() {
         globalThis.game = new WarGame();
@@ -16,17 +21,32 @@ export class WarGame {
     }
 
     async initializeGame() {
+
+        await this._serverList.initializeDB(this._engine.db);
+
         this._gameBoard = new GameBoard();
         await this._gameBoard.initialize();
         this._engine.managers.scene.register(this._gameBoard);
         this._engine.managers.scene.activeScene = this._gameBoard;
 
         this._engine.resumeRender();
+        
+        this._ready = true;
+        this._toRunWhenReady.forEach(runnable => runnable());
+        this._toRunWhenReady = [];
     }
 
-    kill() {
+    async kill() {
         // free all engine gpu memory
-        this._engine.free();
+        await this._engine.free();
+    }
+
+    runWhenReady(runnable: () => void) {
+        if (this._ready) {
+            runnable();
+        } else {
+            this._toRunWhenReady.push(runnable);
+        }
     }
 
     get loader() {
@@ -35,6 +55,10 @@ export class WarGame {
 
     get engine() {
         return this._engine;
+    }
+
+    get serverList() {
+        return this._serverList;
     }
 
 }
