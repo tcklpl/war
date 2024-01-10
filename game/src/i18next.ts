8871ea72994a8ebb31ba84f7f3ab4b906d9c779f
tcklpl <negroniluan@gmail.com>
2024-01-10 10:31:23 -0300

    new localization files for servers

--- game/src/i18next.ts
+++ game/src/i18next.ts
@@ -4,6 +4,8 @@ import countries_enUS from './locales/en-US/countries.json';
 import loading_enUS from './locales/en-US/loading.json';
 import engine_enUS from './locales/en-US/engine.json';
 import config_enUS from "./locales/en-US/config.json";
+import server_list_enUS from "./locales/en-US/server_list.json";
+import common_enUS from "./locales/en-US/common.json";
 
 export const defaultNS = 'ns1';
 
@@ -12,7 +14,9 @@ export const resources = {
         countries: countries_enUS,
         loading: loading_enUS,
         engine: engine_enUS,
-        config: config_enUS
+        config: config_enUS,
+        server_list: server_list_enUS,
+        common: common_enUS
     }
 };
 
