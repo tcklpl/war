9fe5b31a74fa6034929b8f0a7062d164663f6ff2
tcklpl <negroniluan@gmail.com>
2024-01-10 10:39:21 -0300

    player name editor

---
+++ game/src/components/menu/config/player_name_box/player_name_box.tsx
@@ -0,0 +1,44 @@
+import { Divider, IconButton, InputBase, Paper } from "@mui/material";
+import React, { useCallback, useRef, useState } from "react";
+
+import EditIcon from '@mui/icons-material/Edit';
+import { useGameSession } from "../../../../hooks/use_game_session";
+
+
+const PlayerNameBox: React.FC = () => {
+
+    const { username, setUsername, saveGameSession } = useGameSession();
+
+    const [isEditing, setEditing] = useState(false);
+    const inputRef = useRef<HTMLInputElement>();
+
+    const edit = useCallback(() => {
+        setEditing(true);
+        inputRef.current?.focus();
+    }, []);
+
+    const save = useCallback(() => {
+        setEditing(false);
+        saveGameSession();
+    }, [saveGameSession]);
+
+    return (
+        <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }} onSubmit={e => {
+            e.preventDefault();
+            save();
+        }}>
+            <InputBase sx={{ ml: 1, flex: 1 }} 
+                placeholder="Name" 
+                disabled={!isEditing} 
+                ref={inputRef} 
+                value={username} onChange={e => setUsername(e.currentTarget.value)} 
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: '10px' }} onClick={() => (isEditing ? save : edit)()}>
+                <EditIcon/>
+            </IconButton>
+        </Paper>
+    );
+};
+
+export default PlayerNameBox;
