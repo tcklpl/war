46f921e8d1c048d5a94fc7a4fb377ef129c290a1
tcklpl <negroniluan@gmail.com>
2024-01-12 11:54:27 -0300

    basic auth token body interface

--- protocol/index.ts
+++ protocol/index.ts
@@ -1,3 +1,5 @@
 export * from './src/rest/sv_server_info';
 export * from './src/rest/sv_login_response_ok';
 export * from './src/rest/cl_login_request';
+
+export * from './src/auth/auth_token_body';
---
+++ protocol/src/auth/auth_token_body.ts
@@ -0,0 +1,6 @@
+
+export interface AuthTokenBody {
+    username: string;
+    
+    ip: string;
+}
