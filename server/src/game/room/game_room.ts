3b92d5d8c3e77f670a19fdcab0ac4c3585432b53
tcklpl <negroniluan@gmail.com>
2024-01-26 12:09:56 -0300

    base server game room

---
+++ server/src/game/room/game_room.ts
@@ -0,0 +1,23 @@
+import { Player } from "../player/player";
+
+export class GameRoom {
+
+    private _players: Player[] = [];
+    joinable = true;
+
+    constructor(private _owner: Player, private _name: string) {
+        this._players.push(this.owner);
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get players() {
+        return this._players;
+    }
+
+    get owner() {
+        return this._owner;
+    }
+}
