849a7c66fdbc1ab11925bf8b4dba38e0ac51e114
tcklpl <negroniluan@gmail.com>
2024-01-26 12:21:51 -0300

    route to server rooms

--- game/src/components/menu/router/menu_router.tsx
+++ game/src/components/menu/router/menu_router.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import MainMenu from "../main_menu/main_menu";
 import ServerSelectScreen from "../server_select/server_select_screen";
 import CfgMenu from "../config/cfg_menu";
+import ServerRoomSelectScreen from "../server_room_select/server_room_select_screen";
+import GameControlledRoute from "./control/game_controlled_route";
 
 const MenuRouter: React.FC = () => {
 
@@ -12,6 +14,12 @@ const MenuRouter: React.FC = () => {
             <Route path="/" Component={MainMenu}/>
             <Route path="/servers" Component={ServerSelectScreen}/>
             <Route path="/config" Component={CfgMenu}/>
+            
+            <Route path="/rooms" element={
+                <GameControlledRoute requiresActiveSession redirectPath="/servers">
+                    <ServerRoomSelectScreen/>
+                </GameControlledRoute>
+            }/>
         </Routes>
     );
 };
