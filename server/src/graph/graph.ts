374340ca0343deca2a9b579137eac574c4531db0
tcklpl <negroniluan@gmail.com>
2024-02-24 10:29:59 -0300

    initial graph data structure for the map

---
+++ server/src/graph/graph.ts
@@ -0,0 +1,16 @@
+import { Node } from "./node";
+
+export class Graph<T> {
+
+    nodes: Map<T, Node<T>> = new Map();
+
+    addNode(n: T) {
+        let node = this.nodes.get(n);
+        if (node) return node;
+
+        node = new Node(n);
+        this.nodes.set(n, node);
+        return node;
+    }
+
+}
