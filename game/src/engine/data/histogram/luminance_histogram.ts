9186a2ce3b0053ff2c5af883447c16d39110a019
tcklpl <negroniluan@gmail.com>
2024-03-17 19:12:39 -0300

    histogram average calculation

--- game/src/engine/data/histogram/luminance_histogram.ts
+++ game/src/engine/data/histogram/luminance_histogram.ts
@@ -5,6 +5,7 @@ export class LuminanceHistogram {
     readonly bins = 256;
     private _rawData: Uint32Array = new Uint32Array(new Array(this.bins).fill(0));
     private _buffer = BufferUtils.createEmptyBuffer(this.bins * 4, GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ);
+    private _avg: number = 0;
 
     /**
      * Maps and reads the luminance histogram result buffer.
@@ -14,8 +15,13 @@ export class LuminanceHistogram {
         const newData = new Uint32Array(this._buffer.getMappedRange());
         this.updateRawData(newData);
         this._buffer.unmap();
+        this.calculateAverage();
     }
 
+    /**
+     * Replaces the histogram data, checking to see if the new data is of the correct length.
+     * @param data The new histogram data.
+     */
     private updateRawData(data: Uint32Array) {
         if (data.length !== 256) {
             console.warn(`Trying to update a luminance histogram with an array of length ${data.length}, should've been ${this.bins}`);
@@ -24,8 +30,21 @@ export class LuminanceHistogram {
         this._rawData.set(data);
     }
 
+    private calculateAverage() {
+        let total = 0;
+        this._avg = this._rawData.reduce((sum, cur, i) => {
+            const binLDRSlice = i / (this.bins - 1);
+            total += cur;
+            return sum + (cur * binLDRSlice);
+        }, 0) / (total);
+    }
+
     get buffer() {
         return this._buffer;
+    }
+
+    get mean() {
+        return this._avg;
     }
 
     free() {
