95d0ca610db1df29a3ca4d6a02f3dcc32a767187
tcklpl <negroniluan@gmail.com>
2024-04-04 16:48:08 -0300

    initial territory selection server packets

---
+++ server/src/socket/packet/game/ini_territory_selection_assignment.ts
@@ -0,0 +1,9 @@
+import { TerritoryCode } from "../../../../../protocol";
+import { ServerPacket } from "../server_packet";
+
+export class ServerPacketInitialTerritoryAssignment extends ServerPacket<"gInitialTerritorySelectionAssignment"> {
+
+    constructor(player: string, territory: TerritoryCode, reason: 'selected' | 'timeout') {
+        super("gInitialTerritorySelectionAssignment", player, territory, reason);
+    }
+}
