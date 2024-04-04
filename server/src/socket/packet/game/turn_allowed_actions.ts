1ccf93f8b7d21feb20bb47db332f63e70df6c37e
tcklpl <negroniluan@gmail.com>
2024-04-04 16:47:42 -0300

    turn allowed actions package

---
+++ server/src/socket/packet/game/turn_allowed_actions.ts
@@ -0,0 +1,9 @@
+import { TurnAllowedActions } from "../../../../../protocol";
+import { ServerPacket } from "../server_packet";
+
+export class ServerPacketTurnAllowedActions extends ServerPacket<"gTurnAllowedActions"> {
+
+    constructor(pkt: TurnAllowedActions) {
+        super("gTurnAllowedActions", pkt);
+    }
+}
