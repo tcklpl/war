27a4c8382a81edf43ad31c2b22b1fea0b35f73d7
tcklpl <negroniluan@gmail.com>
2024-04-04 16:41:23 -0300

    updated logging utils to have levels

--- server/src/utils/logging_utils.ts
+++ server/src/utils/logging_utils.ts
@@ -1,19 +1,57 @@
 import chalk from "chalk";
 
+export enum LogLevel {
+    ERROR = 0,
+    INFO  = 1,
+    WARN  = 2,
+    DEBUG = 3,
+}
+
 export default class svlog {
+
+    private static _logLevel = LogLevel.INFO;
 
     private static getCurrentTimeString() {
         const time = new Date();
         return chalk.gray(time.toTimeString().split(' ')[0]);
     }
 
-    static log(str: string) {
-        const time = new Date();
+    static parseLogLevelFromString(s: string) {
+        switch (s.trim().toLowerCase()) {
+            case "debug":
+                return LogLevel.DEBUG;
+            case "info":
+                return LogLevel.INFO;
+            case "warn":
+                return LogLevel.WARN;
+            case "error":
+                return LogLevel.ERROR;
+            default:
+                return LogLevel.INFO;
+        }
+    }
+
+    static setLogLevel(l: LogLevel) {
+        this._logLevel = l;
+    }
+
+    static debug(str: string) {
+        if (this._logLevel < LogLevel.DEBUG) return;
+        console.log(`[${this.getCurrentTimeString()}] [${chalk.gray("DEBUG")}] ${str}`);
+    }
+
+    static info(str: string) {
+        if (this._logLevel < LogLevel.INFO) return;
         console.log(`[${this.getCurrentTimeString()}] [${chalk.gray("INFO")}] ${str}`);
     }
 
+    static warn(str: string) {
+        if (this._logLevel < LogLevel.WARN) return;
+        console.log(`[${this.getCurrentTimeString()}] [${chalk.yellow("WARN")}] ${str}`);
+    }
+
     static err(str: string) {
+        if (this._logLevel < LogLevel.ERROR) return;
         console.error(`[${this.getCurrentTimeString()}] [${chalk.red("ERROR")}] ${str}`);
     }
 }
-
