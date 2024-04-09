aac2988379ea22c9ac365c67a6bb32153d4f2781
tcklpl <negroniluan@gmail.com>
2024-04-09 17:53:26 -0300

    implementing abstract methods on other parties

--- server/src/game/party/capitalism.ts
+++ server/src/game/party/capitalism.ts
@@ -1,8 +1,21 @@
+import { TerritoryCode, TurnAllowedActions, TurnPhase } from "../../../../protocol";
 import { Party } from "./party";
 
 export class PartyCapitalism extends Party {
 
     constructor() {
         super("capitalism");
+    }
+
+    get startingTerritories(): "any" | TerritoryCode[] {
+        return [ "california" ];
+    }
+
+    calculateAllowedTurnActionsForPhase(phase: TurnPhase): TurnAllowedActions {
+        throw new Error("Method not implemented.");
+    }
+
+    checkWinCondition(): boolean {
+        return false;
     }
 }
--- server/src/game/party/feudalism.ts
+++ server/src/game/party/feudalism.ts
@@ -1,3 +1,4 @@
+import { TerritoryCode, TurnAllowedActions, TurnPhase } from "../../../../protocol";
 import { Party } from "./party";
 
 export class PartyFeudalism extends Party {
@@ -5,4 +6,17 @@ export class PartyFeudalism extends Party {
     constructor() {
         super("feudalism");
     }
+
+    get startingTerritories(): "any" | TerritoryCode[] {
+        return [ "france" ];
+    }
+
+    calculateAllowedTurnActionsForPhase(phase: TurnPhase): TurnAllowedActions {
+        throw new Error("Method not implemented.");
+    }
+
+    checkWinCondition(): boolean {
+        return false;
+    }
+    
 }
