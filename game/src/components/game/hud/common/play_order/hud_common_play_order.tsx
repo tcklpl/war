5d97d84906edb0a5e3b45903867785e0e0e03d74
tcklpl <negroniluan@gmail.com>
2024-04-09 17:54:16 -0300

    basic play order component

---
+++ game/src/components/game/hud/common/play_order/hud_common_play_order.tsx
@@ -0,0 +1,40 @@
+import { Box, Tab, Tabs } from "@mui/material";
+import { FunctionComponent, ReactElement } from "react";
+import { useGameSession } from "../../../../../hooks/use_game_session";
+import { GameParty } from "../../../../../../../protocol";
+import { useTranslation } from "react-i18next";
+
+import AnarchismIcon from "../../../../../images/icons/anarchism/anarchism_icon";
+import FeudalismIcon from "../../../../../images/icons/feudalism/feudalism_ison";
+import SocialismIcon from "../../../../../images/icons/socialism/socialism_ison";
+import CapitalismIcon from "../../../../../images/icons/capitalism/capitalism_ison";
+
+interface HUDCommonPlayOrderProps {
+    
+}
+ 
+const HUDCommonPlayOrder: FunctionComponent<HUDCommonPlayOrderProps> = () => {
+
+    const { currentGameSession, gTurnPlayerIndex } = useGameSession();
+    const { t } = useTranslation(["parties"]);
+
+    const partyDecoratorMap = new Map<GameParty, {name: string, icon: ReactElement}>([
+        ["anarchism", { name: t("parties:anarchism"), icon: (<AnarchismIcon/>)}],
+        ["feudalism", { name: t("parties:feudalism"), icon: (<FeudalismIcon/>)}],
+        ["socialism", { name: t("parties:socialism"), icon: (<SocialismIcon/>)}],
+        ["capitalism", { name: t("parties:capitalism"), icon: (<CapitalismIcon/>)}],
+    ]);
+
+    if (!currentGameSession) return <></>;
+    return (
+        <Box width="100%" height="100%" display="flex" flexDirection="column" bgcolor="background.paper">
+            <Tabs value={gTurnPlayerIndex} orientation="vertical">
+                { currentGameSession.initialGameState.players.map((p, i) => (
+                    <Tab label={p.name} icon={partyDecoratorMap.get(p.party)?.icon} iconPosition="top" key={i}/>
+                ))}
+            </Tabs>
+        </Box>
+    );
+}
+ 
+export default HUDCommonPlayOrder;
