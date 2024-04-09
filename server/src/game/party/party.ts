06b94d088f9a1656ec76e245004f79ad5193ba7e
tcklpl <negroniluan@gmail.com>
2024-04-09 17:52:24 -0300

    initial party methods

--- server/src/game/party/party.ts
+++ server/src/game/party/party.ts
@@ -1,12 +1,70 @@
-import { GameParty } from "../../../../protocol";
+import { GameParty, TerritoryCode, TurnAllowedActionMoveTroops, TurnAllowedActions, TurnPhase } from "../../../../protocol";
 import { Player } from "../player/player";
+import { Territory } from "../territory/territory";
 
 export abstract class Party {
 
     player?: Player;
+    private _territories: Territory[] = [];
+    availableTroops = 0;
+    readonly turnPhaseSequence: TurnPhase[] = [ "troop positioning", "attack", "over" ]
 
     constructor(
         readonly protocolValue: GameParty
     ) {}
+
+    abstract get startingTerritories(): 'any' | TerritoryCode[];
+    
+    addTerritory(t: Territory) {
+        if (!this._territories.some(x => x === t)) {
+            this._territories.push(t);
+            t.setParty(this);
+        }
+    }
+
+    removeTerritory(t: Territory) {
+        if (this._territories.some(x => x === t)) {
+            this._territories = this._territories.filter(x => x !== t);
+            t.setParty(undefined);
+        }
+    }
+
+    protected generateActionID() {
+        return crypto.randomUUID();
+    }
+
+    onRoundStart() {}
+    onTurnStart() {}
+    abstract calculateAllowedTurnActionsForPhase(phase: TurnPhase): TurnAllowedActions;
+    onTurnEnd() {}
+    onRoundEnd() {}
+
+    protected calculatePossibleTroopMovement(): TurnAllowedActionMoveTroops {
+        return {
+            id: this.generateActionID(),
+            action_forces_phase_change: false,
+            movement: this._territories.flatMap(t => t.node.adjacentNodes.map(a => {
+                return {
+                    from: t.code,
+                    to: a.data.code,
+                    count: t.troops.length
+                }
+            }))
+        }
+    }
+
+    abstract checkWinCondition(): boolean;
+
+    get territories() {
+        return this._territories;
+    }
+
+    get numberOfTroopsPerTurn() {
+        return Math.max(Math.floor(this._territories.length / 2), 1);
+    }
+
+    get targetableTerritories() {
+        return this._territories.flatMap(t => t.node.adjacentNodes).filter(t => t.data.party !== this).map(t => t.data);
+    }
 
 }
