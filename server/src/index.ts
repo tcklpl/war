96b963eff8ac1ddbe5a5ac0c91409263436021d2
tcklpl <negroniluan@gmail.com>
2024-04-26 17:24:33 -0300

    server graceful shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,3 +2,18 @@ import { WarServer } from "./war_server";
 
 const server = new WarServer();
 server.initialize();
+
+// When the process is killed
+process.on('SIGTERM', async () => {
+    await server.stop();
+});
+
+// When the process is interrupted (^C)
+process.on('SIGINT', async () => {
+    await server.stop();
+});
+
+// Nodemon reload
+process.on('SIGUSR2', async () => {
+    await server.stop();
+});
