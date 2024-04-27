00ba8a7c6323f6c8ea8d7ee740ede875e7015ccc
tcklpl <negroniluan@gmail.com>
2024-04-26 22:18:23 -0300

    credits button on main menu

--- game/src/components/menu/main_menu/main_menu.tsx
+++ game/src/components/menu/main_menu/main_menu.tsx
@@ -1,18 +1,19 @@
 import { Button, Grid, Stack } from "@mui/material";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-import PublicIcon from '@mui/icons-material/Public';
-import SettingsIcon from '@mui/icons-material/Settings';
 import PlayerNameBox from "../config/player_name_box/player_name_box";
 import { useGameSession } from "../../../hooks/use_game_session";
 import { useTranslation } from "react-i18next";
+
+import PublicIcon from '@mui/icons-material/Public';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
 const MainMenu: React.FC = () => {
 
     const { username } = useGameSession();
     const navigate = useNavigate();
-    const { t } = useTranslation(["server_list", "config"]);
+    const { t } = useTranslation(["server_list", "config", "credits"]);
 
     return (
         <Grid container style={{ height: '100vh' }} justifyContent="center" alignContent="center">
@@ -20,6 +21,7 @@ const MainMenu: React.FC = () => {
                 <PlayerNameBox/>
                 <Button variant="outlined" startIcon={<PublicIcon/>} fullWidth={true} disabled={!username} onClick={() => navigate("/servers")}>{ t("server_list:server_list") }</Button>
                 <Link to="/config"><Button variant="outlined" startIcon={<SettingsIcon/>} fullWidth={true}>{ t("config:config") }</Button></Link>
+                <Link to="/credits"><Button variant="outlined" startIcon={<MenuBookIcon/>} fullWidth={true}>{ t("credits:credits") }</Button></Link>
             </Stack>
         </Grid>
     );
