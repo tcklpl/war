e4d44356cb23070583ae51fd3c95ba668a67d1f3
tcklpl <negroniluan@gmail.com>
2024-05-02 19:32:36 -0300

    removed unused import

--- game/src/@types/socket.ts
+++ game/src/@types/socket.ts
@@ -1,5 +1,5 @@
 import { Socket } from "socket.io-client";
-import { ClientToServerPackets, ServerToClientPackets, SocketData } from "../../../protocol";
+import { ClientToServerPackets, ServerToClientPackets } from "../../../protocol";
 
 
 export type GameSocket = Socket<ServerToClientPackets, ClientToServerPackets>;
