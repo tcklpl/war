7814d9e6d013146b2726c7259ba35d7cb00dd411
tcklpl <negroniluan@gmail.com>
2024-05-13 17:50:56 -0300

    made renderer free async

--- game/src/engine/render/renderer.ts
+++ game/src/engine/render/renderer.ts
@@ -3,6 +3,6 @@ export abstract class Renderer {
 
     abstract initialize(): Promise<void>;
     abstract render(): Promise<void>;
-    abstract free(): void;
+    abstract free(): Promise<void>;
 
 }
