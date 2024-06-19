75a140c4e271547c64f6cc5dad2e3d07d830fa9c
tcklpl <negroniluan@gmail.com>
2024-06-19 20:49:37 -0300

    server now also returns socket port

--- server/src/express/routes/route_server_info.ts
+++ server/src/express/routes/route_server_info.ts
@@ -1,18 +1,18 @@
-import { sv_ServerInfo } from "../../../../protocol";
-import { CfgServer } from "../../config/default/cfg_server";
-import { ExpressRoute } from "./route";
+import { ResponseServerInfoBody } from '../../../../protocol';
+import { CfgServer } from '../../config/default/cfg_server';
+import { ExpressRoute } from './route';
 
 export class RouteServerInfo extends ExpressRoute {
-
     register() {
         const serverConfig = this._configManager.getConfig(CfgServer);
         this.router.get('/', (req, res) => {
-            res.status(200).json(<sv_ServerInfo> {
+            res.status(200).json(<ResponseServerInfoBody>{
                 name: serverConfig.name,
-                hasPassword: serverConfig.password !== "",
+                hasPassword: serverConfig.password !== '',
                 description: serverConfig.description,
+                socketPort: serverConfig.socket_port,
                 playerLimit: serverConfig.max_players,
-                playerCount: this._gameServer.playerManager.loggedPlayers.length
+                playerCount: this._gameServer.playerManager.loggedPlayers.length,
             });
         });
     }
