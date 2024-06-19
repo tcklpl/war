5a28d1064e98177bfd584704afe300f7405b9755
tcklpl <negroniluan@gmail.com>
2024-06-19 20:55:46 -0300

    main menu reconnect to server

--- game/src/components/menu/main_menu/main_menu.tsx
+++ game/src/components/menu/main_menu/main_menu.tsx
@@ -1,23 +1,62 @@
-import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import { Box, Button, ButtonGroup, Grid, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import PlayerNameBox from '../config/player_name_box/player_name_box';
 import { useGameSession } from '../../../hooks/use_game_session';
 import { useTranslation } from 'react-i18next';
+import { useGame } from '../../../hooks/use_game';
+import { useAlert } from '../../../hooks/use_alert';
 
 import PublicIcon from '@mui/icons-material/Public';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
+import RestoreIcon from '@mui/icons-material/Restore';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useConfirmation } from '../../../hooks/use_confirmation';
 
 const MainMenu: React.FC = () => {
-    const { username } = useGameSession();
+    const { username, reconnectionInfo, setReconnectionInfo } = useGameSession();
+    const { gameInstance } = useGame();
+    const { enqueueAlert } = useAlert();
+    const { enqueueConfirmation } = useConfirmation();
     const navigate = useNavigate();
-    const { t } = useTranslation(['server_list', 'config', 'credits']);
+    const { t } = useTranslation(['server_list', 'config', 'credits', 'common']);
+
+    const reconnectToLastSession = async () => {
+        if (!reconnectionInfo) return;
+        const success = await gameInstance?.state.reconnectToServer(reconnectionInfo);
+        if (!success) {
+            enqueueAlert({
+                title: t('common:failed_to_rejoin'),
+                content: t('common:error_failed_to_rejoin_socket'),
+            });
+        }
+    };
+
+    const deleteLastSession = () => {
+        enqueueConfirmation({
+            title: t('common:delete_last_session'),
+            description: t('common:delete_last_session_desc'),
+            onConfirm() {
+                setReconnectionInfo();
+            },
+        });
+    };
 
     return (
         <Grid container style={{ height: '100vh' }} justifyContent='center' alignContent='center'>
             <Stack spacing={2}>
                 <PlayerNameBox />
+                {reconnectionInfo && (
+                    <ButtonGroup variant='outlined' sx={{ display: 'flex' }}>
+                        <Button startIcon={<RestoreIcon />} sx={{ flex: '1' }} onClick={() => reconnectToLastSession()}>
+                            {t('common:reconnect')}
+                        </Button>
+                        <Button onClick={() => deleteLastSession()}>
+                            <DeleteIcon />
+                        </Button>
+                    </ButtonGroup>
+                )}
                 <Button
                     variant='outlined'
                     startIcon={<PublicIcon />}
