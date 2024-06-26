1a2bca557f51f5513ac2b738612664e738901f3d
tcklpl <negroniluan@gmail.com>
2024-06-26 18:06:08 -0300

    proper protocol index

--- protocol/src/protocol/data/ingame/index.ts
+++ protocol/src/protocol/data/ingame/index.ts
@@ -1,2 +1,6 @@
 export * from './game_action';
+export * from './game_error';
+export * from './game_stage';
+export * from './premature_game_end_reason';
+export * from './reconnection_status';
 export * from './round';
