dae67900cabe8a09e2dd183803813e8a8a8ceef5
tcklpl <negroniluan@gmail.com>
2024-06-26 18:05:55 -0300

    moved game save construction to entity

--- server/src/persistence/model/game_save.ts
+++ server/src/persistence/model/game_save.ts
@@ -1,5 +1,6 @@
 import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { GamePlayerSave } from './game_player_save';
+import { Game } from '../../game/ingame/game';
 
 @Entity()
 export class GameSave {
@@ -8,4 +9,10 @@ export class GameSave {
 
     @OneToMany(() => GamePlayerSave, player => player.game)
     players!: GamePlayerSave[];
+
+    fromGame(game: Game) {
+        this.id = game.id;
+        this.players = game.players.map(p => new GamePlayerSave().fromGamePlayerAndGameSave(p, this));
+        return this;
+    }
 }
--- server/src/persistence/service/game_save_service.ts
+++ server/src/persistence/service/game_save_service.ts
@@ -1,6 +1,5 @@
 import { Game } from '../../game/ingame/game';
 import { GameSaveDao } from '../dao/game_save_dao';
-import { GamePlayerSave } from '../model/game_player_save';
 import { GameSave } from '../model/game_save';
 import { PersistenceManager } from '../persistence_manager';
 
@@ -11,15 +10,7 @@ export class GameSaveService {
         this._gsDao = pm.dao.gameSave;
     }
 
-    private generateSaveFromGame(g: Game) {
-        const gameSave = new GameSave();
-        const gsPlayers = g.players.map(p => new GamePlayerSave().fromGamePlayerAndGameSave(p, gameSave));
-        gameSave.players = gsPlayers;
-        gameSave.id = g.id;
-        return gameSave;
-    }
-
     async save(game: Game) {
-        this._gsDao.save(this.generateSaveFromGame(game));
+        this._gsDao.save(new GameSave().fromGame(game));
     }
 }
