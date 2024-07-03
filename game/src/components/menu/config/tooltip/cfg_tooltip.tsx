11f37ee3a40fb644f2f1f07fb7fe0863fb9d862d
tcklpl <negroniluan@gmail.com>
2024-07-03 20:39:50 -0300

    scss modules on tooltip

--- game/src/components/menu/config/tooltip/cfg_tooltip.tsx
+++ game/src/components/menu/config/tooltip/cfg_tooltip.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
-import './cfg_tooltip.scss';
+import styles from './cfg_tooltip.module.scss';
 
 const CfgTooltip: React.FC<{ currentTooltip?: { title: string; content: string } }> = ({ currentTooltip }) => {
     return (
-        <Stack spacing={2} className='cfg-tooltip'>
-            <Typography variant='h6' className='cfg-tooltip-title'>
-                {currentTooltip?.title}
-            </Typography>
-            <Typography variant='body1' className='cfg-tooltip-body'>
+        <Stack spacing={2} className={styles.main}>
+            <Typography variant='h6'>{currentTooltip?.title}</Typography>
+            <Typography variant='body1' className={styles.body}>
                 {currentTooltip?.content}
             </Typography>
         </Stack>
