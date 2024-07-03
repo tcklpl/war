cc48ecf173e9e3827ff3cfdfbd56afc536c38053
tcklpl <negroniluan@gmail.com>
2024-07-03 20:40:21 -0300

    nested cfg_default_background

--- game/src/components/menu/config/default_background/cfg_default_background.tsx
+++ game/src/components/menu/config/default_background/cfg_default_background.tsx
@@ -1,5 +1,5 @@
 import { Grid, Stack, Typography, useTheme } from '@mui/material';
-import './cfg_default_background.scss';
+import style from './cfg_default_background.module.scss';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useTranslation } from 'react-i18next';
 
@@ -12,11 +12,11 @@ const CfgScreenDefaultBackground: React.FC = () => {
             container
             direction='column'
             style={{ backgroundColor: palette.background.default }}
-            className='cfg-default-screen'
+            className={style.screen}
             justifyContent='center'
             alignItems='center'
         >
-            <Stack spacing={2} className='cfg-default-screen-content' alignItems='center'>
+            <Stack spacing={2} className={style.content} alignItems='center'>
                 <SettingsIcon sx={{ fontSize: 100 }} />
                 <Typography variant='h4'>{t('config:default_screen_header')}</Typography>
                 <Typography variant='body1'>{t('config:default_screen_body')}</Typography>
