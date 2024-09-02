8ae376a0597589c65233817cd052350fcc485c4b
tcklpl <negroniluan@gmail.com>
2024-09-01 21:10:24 -0300

    prettier

--- server/src/banner.ts
+++ server/src/banner.ts
@@ -1,15 +1,12 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
 export class WarServerBanner {
-
-    readonly version = "0.0.1";
-    readonly version_type: "Development" | "Release" = "Development";
-    readonly source_url = "https://github.com/tcklpl/war";
+    readonly version = process.env.SERVER_VERSION;
+    readonly version_type: 'Development' | 'Release' = 'Development';
+    readonly source_url = 'https://github.com/tcklpl/war';
     readonly greetings = `
         War Server ${chalk.gray(this.version)} (${chalk.gray(this.version_type)})
-        ${chalk.dim("Source available at " + chalk.underline(this.source_url))}
-        ${chalk.dim("Enter help to see the command list.")}
+        ${chalk.dim('Source available at ' + chalk.underline(this.source_url))}
+        ${chalk.dim('Enter help to see the command list.')}
     `;
-
-
 }
