94c94895823b2ce4c17ff0cdcc3cd7c96f04e7c7
tcklpl <negroniluan@gmail.com>
2024-11-01 20:32:35 -0300

    animation errors

---
+++ game/src/errors/engine/data/animation/invalid_interpolation_type.ts
@@ -0,0 +1,3 @@
+import { EngineError } from '../../engine_error';
+
+export class InvalidInterpolationTypeError extends EngineError {}
---
+++ game/src/errors/engine/data/animation/unsupported_animation_step_value.ts
@@ -0,0 +1,3 @@
+import { EngineError } from '../../engine_error';
+
+export class UnsupportedAnimationStepValueError extends EngineError {}
