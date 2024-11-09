a734f0b5dfcca476398acd80ea5adec102e26abb
tcklpl <negroniluan@gmail.com>
2024-11-09 11:58:24 -0300

    board country animation triggers

--- game/src/game/board/board_country.ts
+++ game/src/game/board/board_country.ts
@@ -4,10 +4,14 @@ import { PointLight } from '../../engine/data/lights/point_light';
 import { interactable } from '../../engine/data/traits/interactable';
 import { Vec3 } from '../../engine/data/vec/vec3';
 import { MissingAssetError } from '../../errors/engine/asset/missing_asset';
+import {
+    BoardCountryAnimationHoverOn,
+    BoardCountryAnimationResetPosition,
+} from './animations/board_country_animations';
 
 const BoardCountryBase = interactable(Entity);
 export class BoardCountry extends BoardCountryBase {
-    private _hoverLight = new PointLight(
+    private readonly _hoverLight = new PointLight(
         {
             color: Vec3.fromValue(1),
             intensity: 10,
@@ -20,11 +24,16 @@ export class BoardCountry extends BoardCountryBase {
         },
         false,
     );
-    private _lightOffset = new Vec3(0, -0.1, 0);
+    private readonly _lightOffset = new Vec3(0, -0.1, 0);
+
+    private readonly animationTable = {
+        hoverOn: BoardCountryAnimationHoverOn(this),
+        resetPosition: BoardCountryAnimationResetPosition(this, this.translation),
+    };
 
     constructor(
         name: string,
-        private _gltfName: string,
+        private readonly _gltfName: string,
     ) {
         const meshNode = game.engine.managers.asset
             .getGLTFAsset('board')
@@ -42,17 +51,27 @@ export class BoardCountry extends BoardCountryBase {
         this.onTransform(() => {
             this._hoverLight.properties.position = this.translation.add(this._lightOffset);
         });
+
+        this.updateAnimationTable();
     }
 
     onMouseHover(): void {
         this.overlayIntensity = 1;
         this.overlayColor = new Vec3(5, 0, 0);
         this._hoverLight.enabled = true;
+        this.puppeteer.cancelAllAnimations();
+        this.puppeteer.playAnimation(this.animationTable.hoverOn);
     }
 
     onMouseLeave(): void {
         this.overlayIntensity = 0;
         this._hoverLight.enabled = false;
+        this.puppeteer.cancelAllAnimations();
+        this.puppeteer.playAnimation(this.animationTable.resetPosition);
+    }
+
+    private updateAnimationTable() {
+        this.animationTable.resetPosition = BoardCountryAnimationResetPosition(this, this.translation);
     }
 
     get hoverLight() {
