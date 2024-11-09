00a84a113e4b3a004e6194f78ed2db050737834b
tcklpl <negroniluan@gmail.com>
2024-11-09 16:09:55 -0300

    moved return button into cfg screen

--- game/src/components/menu/config/menu/cfg_menu.tsx
+++ game/src/components/menu/config/menu/cfg_menu.tsx
@@ -1,24 +1,12 @@
-import { Button, Grid2, Stack } from '@mui/material';
+import { Grid2 } from '@mui/material';
 import React from 'react';
 import CfgScreen from '../screen/cfg_screen';
 
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
-
 const CfgMenu: React.FC = () => {
-    const navigate = useNavigate();
-    const { t } = useTranslation(['common']);
-
     return (
         <Grid2 container style={{ height: '100vh' }} justifyContent='center' alignContent='start'>
-            <Grid2 size={{ xs: 8 }} alignSelf='center'>
-                <Stack>
-                    <Button variant='outlined' startIcon={<ArrowBackIcon />} onClick={() => navigate('/')}>
-                        {t('common:back_to_menu')}
-                    </Button>
-                    <CfgScreen />
-                </Stack>
+            <Grid2 size={{ xs: 8 }} alignSelf='center' height={'100%'}>
+                <CfgScreen showReturnToMenu />
             </Grid2>
         </Grid2>
     );
