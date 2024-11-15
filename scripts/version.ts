44a89354e8dddeb6f29004ac8b923ef11e888060
tcklpl <negroniluan@gmail.com>
2024-11-15 16:53:29 -0300

    version script

---
+++ scripts/version.ts
@@ -0,0 +1,53 @@
+import { $ } from "bun";
+
+// Check for uncommitted changes
+const status = await $`git status --porcelain`.quiet();
+if (status.text()) {
+  console.error("There are uncommitted changes.");
+  process.exit(1);
+}
+
+const newVersion = Bun.argv[3] ?? Bun.argv[2];
+
+if (!newVersion) {
+  console.error("Please enter a version number.");
+  process.exit(1);
+}
+
+const versionParts = newVersion.split(".").filter((p) => !!p);
+if (versionParts.length !== 3) exitInvalidVersion();
+let [major, minor, patchAndMore]: string[] | number[] = versionParts;
+
+const patchParts = patchAndMore.split("-").filter((p) => !!p);
+if (patchParts.length !== 2) exitInvalidVersion();
+
+let [patch, extra]: string[] | number[] = patchParts;
+try {
+  [major, minor, patch] = [major, minor, patch].map((v) => parseInt(v));
+  if (!major || !minor || !patch) throw Error();
+} catch {
+  exitInvalidVersion();
+}
+
+function exitInvalidVersion() {
+  console.error("Please enter a valid version number");
+  process.exit(1);
+}
+
+let finalVersion = [major, minor, patch].join(".");
+if (extra) finalVersion += `-${extra}`;
+
+const packages = [
+  "./game/package.json",
+  "./server/package.json",
+  "./protocol/package.json",
+];
+
+for (const pkg of packages) {
+  const pkgJson = await Bun.file(pkg).json();
+  pkgJson.version = finalVersion;
+  await Bun.write(pkg, JSON.stringify(pkgJson, null, 2));
+  await $`git add ${pkg}`;
+}
+
+export {};
