4b5889e539195751fd79aa928a0d7abf994a41c1
tcklpl <negroniluan@gmail.com>
2024-11-23 14:59:07 -0300

    directional light buffer

--- game/src/engine/data/lights/directional_light.ts
+++ game/src/engine/data/lights/directional_light.ts
@@ -1,10 +1,24 @@
 import { MathUtils } from '../../../utils/math_utils';
-import { Mat4 } from '../mat/mat4';
 import { Quaternion } from '../quaternion/quaternion';
-import { Vec3 } from '../vec/vec3';
-import { Vec4 } from '../vec/vec4';
 import { Light, LightProperties } from './light';
 
+/*
+    Directional light buffer format (0x70 Bytes):
+
+    0   4   8   B   F
+00  ░░░ color ░░░▒▒▒▒   [vec3f "color" (12 bytes)] + [f32 "radius" (4 bytes)]
+10  ░ direction ░▒▒▒▒   [vec3f "direction" (12 bytes)] + [f32 "intensity" (4 bytes)]
+20  ▓ shadow map uv ▓   [vec4f "shadow map UV" (16 bytes)]
+30  |               |
+40  |     Shadow    |   [mat4x4f "shadow viewproj matrix" (64 bytes)]
+50  |ViewProj Matrix|
+60  |               |
+70
+    -: padding
+    ░: vec3f
+    ▒: f32
+    ▓: vec4f
+*/
 export class DirectionalLight extends Light {
     constructor(
         props: LightProperties,
@@ -15,45 +29,30 @@ export class DirectionalLight extends Light {
     }
 
     static get byteSize() {
-        return MathUtils.ensurePadding(2 * Vec3.byteSize + 4 + Vec4.byteSize + Mat4.byteSize);
+        return MathUtils.ensurePadding(304);
+    }
+
+    private getShadowMappingData(): number[] {
+        return this.shadowMappingViewProj?.values ?? new Array(0x40).fill(0);
     }
 
     writeToBuffer(buf: GPUBuffer, index: number, generalBufferOffset: number): void {
         const offset = index * DirectionalLight.byteSize + generalBufferOffset;
 
-        /*
-            Directional light buffer format:
-
-            0   4   8   B   F
-            ░░░ color ░░░----   [vec3f "color" (12 bytes)] + [padding (4 bytes)]
-            ░ direction ░▒▒▒▒   [vec3f "direction" (12 bytes)] + [f32 "intensity" (4 bytes)]
-            ▓ shadow map uv ▓   [vec4f "shadow map UV" (16 bytes)]
-            |               |
-            |     Shadow    |   [mat4x4f "shadow viewproj matrix" (64 bytes)]
-            |ViewProj Matrix|
-            |               |
-
-            -: padding
-            ░: vec3f
-            ▒: f32
-            ▓: vec4f
-        */
-        device.queue.writeBuffer(buf, offset, this.properties.color.asF32Array);
-        device.queue.writeBuffer(
-            buf,
-            offset + Vec3.byteSize + 4,
-            this.rotation.asDirectionVector.multiplyFactor(-1).normalize().asF32Array,
-        );
-        device.queue.writeBuffer(buf, offset + Vec3.byteSize * 2 + 4, new Float32Array([this.properties.intensity]));
         const shadowMapUV = [
             this.shadowAtlasMappedRegion?.uvLowerCorner.x ?? -1,
             this.shadowAtlasMappedRegion?.uvLowerCorner.y ?? -1,
             this.shadowAtlasMappedRegion?.uvHigherCorner.x ?? -1,
             this.shadowAtlasMappedRegion?.uvHigherCorner.y ?? -1,
         ];
-        device.queue.writeBuffer(buf, offset + 0x20, new Float32Array(shadowMapUV));
-        if (this.shadowMappingViewProj) {
-            device.queue.writeBuffer(buf, offset + 0x30, this.shadowMappingViewProj.asF32Array);
-        }
+        const data = Float32Array.of(
+            ...this.properties.color.values,
+            this.properties.radius,
+            ...this.rotation.asDirectionVector.multiplyFactor(-1).normalize().values,
+            this.properties.intensity,
+            ...shadowMapUV,
+            ...this.getShadowMappingData(),
+        );
+        device.queue.writeBuffer(buf, offset, data);
     }
 }
