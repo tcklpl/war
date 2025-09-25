997aa00e8bf696a31b3e716b562b59ad9108e56e
tcklpl <negroniluan@gmail.com>
2025-09-25 16:24:39 -0300

    removed render stage interface

--- game/src/engine/render/renderer/vanilla/render-stages/render-stage.ts
+++
@@ -1,9 +0,0 @@
-import type { RenderResourcePool } from '../render-resource-pool';
-
-export interface RenderStage {
-	initialize?: (pool: RenderResourcePool) => Promise<void>;
-	render: (pool: RenderResourcePool) => void | Promise<void>;
-	free?: () => void;
-
-	onScreenResize?: (pool: RenderResourcePool) => void;
-}
