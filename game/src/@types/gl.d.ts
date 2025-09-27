8253b7f7eb99634a5699e6d85b6b9ca3167ef963
tcklpl <negroniluan@gmail.com>
2025-09-27 12:23:54 -0300

    removed a lot of globals

--- game/src/@types/gl.d.ts
+++ game/src/@types/gl.d.ts
@@ -1,11 +1,7 @@
 import type { WarGame } from ':game/war-game';
 
 declare global {
-	var gl: WebGL2RenderingContext;
-	var device: GPUDevice;
-	var gameCanvas: HTMLCanvasElement;
 	var game: WarGame;
-	var gpuCtx: GPUCanvasContext;
 }
 
 export default global;
